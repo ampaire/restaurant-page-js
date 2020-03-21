a51e3fdf15b57e551c55e8f7dc30aa0eb6f7a368
ampaire <femayapr@gmail.com>
2020-03-21 09:34:17 -0600

    Finished working on the homepage

--- src/modules/home.js
+++ src/modules/home.js
@@ -3,11 +3,13 @@ const homepage = () => {
   items.id = "home";
   items.classList.add("homepage", "homepage-fluid");
   const container = document.createElement("div");
-  container.classList.add("container", "text-danger");
+  container.classList.add("container", "text-white");
   container.innerHTML = `
-  <h1 class="display-4 ml-4"> Jerky's </h1>
-  <p class="lead"><strong>It's finger licking good</strong></p>
+  <h1 class="display-4 ml-4 welcome"> Jerky's </h1>
+  <p class="phrase"><strong>It's finger licking good !!</strong></p>
   <hr class="my-4">
+  <p class="phrase"><strong>Satisfy your bear appetite Today.</strong></p>
+  <p class="phrase"><strong>Don't forget to check for our Today's special</strong></p>
   <p> </p>`;
   items.appendChild(container);
 
