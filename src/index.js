22acfdbb6964769d888bd76d7263a2c3bb1ea1a9
ampaire <femayapr@gmail.com>
2020-03-24 14:14:11 -0600

    Add drinks to the data to be added in the menu

--- src/index.js
+++ src/index.js
@@ -9,4 +9,29 @@ const start = () => {
 
 start();
 
+const homePage = document.getElementById('#home');
+const menuPage = document.getElementById('#menu');
+const specialPage = document.getElementById('#special');
+const contactPage = document.getElementById('#contact');
+
+const togglePage = (event) => {
+  if (event.target === homePage) {
+    start();
+  } else if (event.target === menuPage) {
+    addItems.innerHTML = '';
+    addItems.appendChild(menu());
+  } else if (event.target === specialPage) {
+    addItems.innerHTML = '';
+    addItems.appendChild(special());
+  } else {
+    addItems.innerHTML = '';
+    addItems.appendChild(contact());
+  }
+};
+
+homePage.addEventListener('click', togglePage);
+menuPage.addEventListener('click', togglePage);
+specialPage.addEventListener('click', togglePage);
+contactPage.addEventListener('click', togglePage);
+
 
---
+++ src/modules/dummydata.js
@@ -0,0 +1,30 @@
+const menu = () => {
+  const drinks = [
+    {
+      img: 'images/juice.jpg',
+      title: 'Fruit juice',
+      caption: 'Passion, mango, strawberry, pineapple',
+      price: '$8'
+    },
+    {
+      img: 'images/juice2.jpg',
+      title: 'Mixed Fruit juice',
+      caption: 'Natural refreshing mixed fruit juice',
+      price: '$12'
+    },
+    {
+      img: 'images/cocktail.jpg',
+      title: 'Cocktail',
+      caption: 'Classic cocktails',
+      price: '$15'
+    },
+    {
+      img: 'images/beer.jpg',
+      title: 'Beers',
+      caption: 'All kinds of beer',
+      price: 'Prices may vary'
+    }
+  ];
+  
+}
+  export default menu;
